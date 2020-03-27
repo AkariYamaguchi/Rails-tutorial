00f6e654748d5cf8205eb62eafdf43ee1ad84451
AKARI YAMAGUCHI <akr03xxx@gmail.com>
2020-03-27 12:06:03 +0900

    microposts作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_25_050215) do
+ActiveRecord::Schema.define(version: 2020_03_27_030425) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
